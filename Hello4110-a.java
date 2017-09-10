3c2fc64e0403a350ad718211f97ce1ce281cb062
Sofia Cassino <cassino.3@wright.edu>
2017-09-10 16:22:07 -0400

    Committing my local repository

--- Hello4110-a.java
+++ Hello4110-a.java
@@ -1,4 +1,4 @@
-class Hello4110{
+class Hello4110a{
 	public static void main(String[] args){
 		System.out.println("This file is changed again!");
 	}
--- Hello4110-b.java
+++ Hello4110-b.java
@@ -1,4 +1,4 @@
-class Hello4110{
+class Hello4110b{
 	public static void main(String[] args){
 		System.out.println("This file is changed again!");
 	}
